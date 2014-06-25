8babee50ad4e494a87548b878695a9ca31bbd50b
Mandibull <mandibull@gmail.com>
2014-06-26 01:32:21 +0200

    WIP

--- stock_portfolio/views.py
+++ stock_portfolio/views.py
@@ -4,11 +4,11 @@ from models import Stock, Portfolio
 from helpers import get_portfolio_from_id, get_stock_from_symbol
 
 def home(request):
-    return render(request, 'stock_portfolio/home.html', {})
+    return render(request, 'stock_portfolio/home.html', {'portfolios': Portfolio.objects.all()})
 
 @get_portfolio_from_id(fatal=True)
 def portfolio(request, portfolio_id, portfolio):
-        return render(request, 'stock_portfolio/portfolio.html', {})
+        return render(request, 'stock_portfolio/portfolio.html', {'portfolio': portfolio})
 
 # FIXME: move this somewhere else
 def new_stock(symbol):
@@ -21,7 +21,8 @@ def new_stock(symbol):
 def portfolio_add_stock(request, portfolio_id, portfolio, symbol, stock):
     if stock is None:
         sotck = new_stock(symbol)
-    # FIXME: attack stock to portfolio!
+    portfolio.stocks.add(stock)
+    portfolio.save()
     return redirect('portfolio', portfolio_id=portfolio_id)
 
 # TO READ: http://stackoverflow.com/questions/17245498/get-stock-quotes-from-yahoo-finance-in-json-format-using-a-javascript
